0b55b27a65f5950439f3257f20d02a3ca61f5f26
MaksimSamorokovskii <139316826+MaksimSamorokovskii@users.noreply.github.com>
2024-04-05 20:50:28 +0700

    created

---
+++ MyHabitsTracker/MyHabitsTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyHabitsTrackerApp.swift
+//  MyHabitsTracker
+//
+//  Created by Максим Самороковский on 05.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct MyHabitsTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
