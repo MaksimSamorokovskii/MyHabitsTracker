862559cb26610b6a2f1b5ee6dcf74d1f4fa5f094
MaksimSamorokovskii <139316826+MaksimSamorokovskii@users.noreply.github.com>
2024-04-06 13:44:39 +0700

    added

--- MyHabitsTracker/Helpers /Validations.swift
+++ MyHabitsTracker/Helpers /Validations.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+enum Validations {
+    static func validateEmail(_ email: String) -> Bool {
+        let emailRegex = #"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"#
+        
+        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+        return emailPredicate.evaluate(with: email)
+    }
+    
+    static func validatePassword(_ password: String) -> Bool {
+        let passwordRegex = "^(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$"
+        
+        let passwordPredicate = NSPredicate(format: "SELF MATCHES %@", passwordRegex)
+        return passwordPredicate.evaluate(with: password)
+    }
+}
--- MyHabitsTracker/Screens/Login/LoginScreen.swift
+++ MyHabitsTracker/Screens/Login/LoginScreen.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct LoginScreen: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image(.work)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 370)
+        }
     }
 }
 
