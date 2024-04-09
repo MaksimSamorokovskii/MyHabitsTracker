254b24ac9ea0cca0d4e98cc193ab09cc97a1d1fd
MaksimSamorokovskii <139316826+MaksimSamorokovskii@users.noreply.github.com>
2024-04-09 10:48:09 +0700

    LoginScreen

--- MyHabitsTracker/Screens/Login/LoginScreen.swift
+++ MyHabitsTracker/Screens/Login/LoginScreen.swift
@@ -8,12 +8,54 @@
 import SwiftUI
 
 struct LoginScreen: View {
+    @State private var presentNextView = false
     var body: some View {
-        VStack {
-            Image(.work)
-                .resizable()
-                .scaledToFit()
-                .frame(width: 370)
+        NavigationStack {
+            VStack {
+                Image(.work)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 370)
+               Spacer()
+                VStack(spacing: 15) {
+                    Text("Welcome to My Habits Tracker")
+                        .font(.system(size: 25, weight: .bold))
+    //                    .foregroundStyle(.baseBlue)
+   
+                Text("For the application to work correctly, login or register")
+                    .multilineTextAlignment(.center)
+                    .font(.headline)
+                    .foregroundColor(.baseBlue)
+                }
+                Spacer()
+                
+                HStack {
+                    Button {
+                        presentNextView.toggle()
+                    } label: {
+                        Text("Login")
+                            .font(.system(size: 20, weight: .semibold))
+                            .foregroundStyle(.white)
+                    }
+                    .frame(width: 160, height: 50)
+                    .background(.baseBlue)
+                    .clipShape(RoundedRectangle(cornerRadius: 12))
+                    
+                    Button {
+                        // action
+                    } label: {
+                        Text("Registration")
+                            .font(.system(size: 20, weight: .semibold))
+                            .foregroundStyle(.black)
+                    }
+                    .frame(width: 160, height: 50)
+                }
+                Spacer()
+            }
+            .padding()
+            .navigationDestination(isPresented: $presentNextView) {
+                Text("NextView")
+            }
         }
     }
 }
@@ -21,3 +63,4 @@ struct LoginScreen: View {
 #Preview {
     LoginScreen()
 }
+
